0586aeea123283f182d80fb4c7b0d685e4b69d11
Ross Keenan <ross.keenan@protonmail.com>
2022-01-11 19:41:36 -0600

    Added flex for layout and added a bit of spacing as well to clean it up.

--- src/components/MortgageCalculator.vue
+++ src/components/MortgageCalculator.vue
@@ -5,12 +5,28 @@ import { ref } from "vue";
 <template>
   <h3>Mortgage Calculator</h3>
 
-  <label for="homeValue">Home Value</label>
-  <input name="homeValue" type="text" />
+  <div class="mortgageCalcContainer">
+    <div class="containerRow">
+      <label for="homeValue">Home Value</label>
+      <input name="homeValue" type="text" />
+    </div>
+    <div class="containerRow">
+      <label for="homeValue">Home Value</label>
+      <input name="homeValue" type="text" />
+    </div>
+  </div>
 </template>
 
 <style scoped>
 label {
   padding-right: 0.75rem;
+}
+.mortgageCalcContainer {
+  display: flex;
+  flex-direction: column;
+}
+
+.containerRow {
+  margin-bottom: 0.75rem;
 }
 </style>
