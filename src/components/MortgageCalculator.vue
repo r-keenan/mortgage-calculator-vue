b572388a39250f44efff2e7420005f35837cff2d
Ross Keenan <ross.keenan@protonmail.com>
2022-01-11 19:16:19 -0600

    Started working on Mortage Calculator in Vue 3.0.

---
+++ src/components/MortgageCalculator.vue
@@ -0,0 +1,10 @@
+<script setup>
+import { ref } from "vue";
+</script>
+
+<template>
+  <h3>Mortgage Calculator</h3>
+
+  <label for="homeValue">Home Value</label>
+  <input name="homeValue" type="text" />
+</template>
