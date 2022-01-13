6a51a17c94bf2ab9a2790f8273d6d2fbfc0f9e53
Ross Keenan <ross.keenan@protonmail.com>
2022-01-12 19:12:19 -0600

    Adjusted the styling a tad. Business logic next.

--- src/components/MortgageCalculator.vue
+++ src/components/MortgageCalculator.vue
@@ -74,11 +74,18 @@ label {
   padding-right: 0.75rem;
   text-align: left;
 }
+
+h3 {
+  margin-left: 10%;
+}
 .mortgageCalcContainer {
   display: grid;
   grid-template-columns: 1fr 1fr;
   grid-column-gap: 2rem;
   padding-left: 20px;
+  place-self: center;
+  column-gap: 30px;
+  width: 40%;
 }
 
 .containerRow {
