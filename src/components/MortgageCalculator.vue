2199e652e58be07e3fcf719d6948c6f3fcacb22a
Ross Keenan <ross.keenan@protonmail.com>
2022-01-12 19:01:29 -0600

    Removed boilerplate code. stubbed out a form for mortgage calculator.

--- src/components/MortgageCalculator.vue
+++ src/components/MortgageCalculator.vue
@@ -8,11 +8,63 @@ import { ref } from "vue";
   <div class="mortgageCalcContainer">
     <div class="containerRow">
       <label for="homeValue">Home Value</label>
+    </div>
+    <div class="containerRow">
       <input name="homeValue" type="text" />
     </div>
     <div class="containerRow">
-      <label for="homeValue">Home Value</label>
-      <input name="homeValue" type="text" />
+      <label for="downPayment">Down Payment</label>
+    </div>
+    <div class="containerRow">
+      <input name="downPayment" type="text" />
+    </div>
+    <div class="containerRow">
+      <label for="loanAmount">Loan Payment</label>
+    </div>
+    <div class="containerRow">
+      <input name="loanAmount" type="text" />
+    </div>
+    <div class="containerRow">
+      <label for="interestRate">Interest Rate</label>
+    </div>
+    <div class="containerRow">
+      <input name="interestRate" type="text" />
+    </div>
+    <div class="containerRow">
+      <label for="loanTerm">Loan Term (years)</label>
+    </div>
+    <div class="containerRow">
+      <input name="loanTerm" type="text" />
+    </div>
+    <div class="containerRow">
+      <label for="startDate">Start Date</label>
+    </div>
+    <div class="containerRow">
+      <input name="startDate" type="text" />
+    </div>
+    <div class="containerRow">
+      <label for="propertyTax">Property Tax</label>
+    </div>
+    <div class="containerRow">
+      <input name="propertyTax" type="text" />
+    </div>
+    <div class="containerRow">
+      <label for="pmi">PMI</label>
+    </div>
+    <div class="containerRow">
+      <input name="pmi" type="text" />
+    </div>
+    <div class="containerRow">
+      <label for="homeInsurance">Home Insurance</label>
+    </div>
+    <div class="containerRow">
+      <input name="homeInsurance" type="text" />
+    </div>
+    <div class="containerRow">
+      <label for="monthlyHOA">Monthly HOA</label>
+    </div>
+    <div class="containerRow">
+      <input name="monthlyHOA" type="text" />
     </div>
   </div>
 </template>
@@ -20,10 +72,13 @@ import { ref } from "vue";
 <style scoped>
 label {
   padding-right: 0.75rem;
+  text-align: left;
 }
 .mortgageCalcContainer {
-  display: flex;
-  flex-direction: column;
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 2rem;
+  padding-left: 20px;
 }
 
 .containerRow {
